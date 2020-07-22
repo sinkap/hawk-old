ad02621c6571327fb89a903ac295c72c78b38ecd
Carmen-Gabriela Popa <carmengpopa@google.com>
2020-07-22 16:04:39 +0000

    improvements to the script

--- scripts/script.sh
+++ scripts/script.sh
@@ -2,6 +2,17 @@
 
 echo "Open a new terminal and use the following command: sudo cat /sys/kernel/debug/tracing/trace"
 
+kcc()
+{
+  clang -g -D__TARGET_ARCH_x86 -mlittle-endian -Wno-compare-distinct-pointer-types -O2 -target bpf -emit-llvm -c $1 -o - | llc -march=bpf -mcpu=v2 -filetype=obj -o "$(basename  $1 .c).o";
+
+}
+
+ucc ()
+{
+    gcc -g $1 -o "$(basename $1 .c)" -I$HOME/libbpf/src/ $HOME/libbpf/src/libbpf.a -lelf -lz
+}
+
 if test -f ../src/user/user; then
     rm ../src/user/user
 fi
@@ -11,9 +22,11 @@ if test -f ../src/kern.exec.o; then
 fi
 
 bpftool btf dump file /sys/kernel/btf/vmlinux format c > ../src/kern/vmlinux.h
-clang -g -D__TARGET_ARCH_x86 -mlittle-endian -Wno-compare-distinct-pointer-types -O2 -target bpf -emit-llvm -c ../src/kern/exec.c -o - | llc -march=bpf -mcpu=v2 -filetype=obj -o ../src/kern/exec.o;
+kcc ../src/kern/exec.c
+mv  exec.o ../src/kern/exec.o 
 
 bpftool gen skeleton ../src/kern/exec.o > ../src/user/exec.skel.h
-gcc -g ../src/user/user.c -o ../src/user/user -I$HOME/libbpf/src/ $HOME/libbpf/src/libbpf.a -lelf -lz
+ucc ../src/user/user.c
+mv user ../src/user/user
 
 sudo ../src/user/user
